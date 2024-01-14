41d444472c5ebc43f0df67cad8bad77acb7bbfa1
hecsolmay <hecsolmay@gmail.com>
2024-01-14 00:45:22 -0600

    change name in palette color in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,33 +7,33 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: '#72af55',
-        light: '#0d5200'
-      },
-      background: {
-        DEFAULT: '#080808',
-        light: '#e9e9e9'
-      },
-      accent: {
-        DEFAULT: '#1a1a1a',
-        light: '#c2c2c2'
-      },
-      contrast: {
-        DEFAULT: '#4d4d4d',
-        light: '#a5a5a5'
-      },
-      highlight: {
-        DEFAULT: '#b3b3b3',
-        light: '#F8F8F8'
-      }
-    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        primary: {
+          dark: '#72af55',
+          DEFAULT: '#0d5200'
+        },
+        background: {
+          dark: '#0f0f0f',
+          DEFAULT: '#e9e9e9'
+        },
+        accent: {
+          dark: '#1a1a1a',
+          DEFAULT: '#c2c2c2'
+        },
+        contrast: {
+          dark: '#4d4d4d',
+          DEFAULT: '#a5a5a5'
+        },
+        highlight: {
+          dark: '#b3b3b3',
+          DEFAULT: '#F8F8F8'
+        }
       }
     }
   },
