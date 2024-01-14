a83a9f35c91ca64ef89341dcc5fd9ce4a7ed78fa
hecsolmay <hecsolmay@gmail.com>
2024-01-14 00:49:48 -0600

    add theme button to change color scheme

---
+++ src/components/buttons/theme-button.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import MoonIcon from '@/components/icons/moon'
+import SunIcon from '@/components/icons/sun'
+import { useTheme } from 'next-themes'
+
+export default function ThemeButton () {
+  const { setTheme, theme } = useTheme()
+
+  const handleClick = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
+  const isDarkMode = theme === 'dark'
+
+  return (
+    <button title={isDarkMode ? 'Cambiar a modo claro' : 'Cambiar a modo oscuro'} onClick={handleClick}>
+      {isDarkMode ? <SunIcon className='size-7' /> : <MoonIcon className='size-7'/>}
+    </button>
+  )
+}
