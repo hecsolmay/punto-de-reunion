85b32d5cac87b549765f24f8e1d68e8c2433fdfe
hecsolmay <hecsolmay@gmail.com>
2024-01-14 19:38:51 -0600

    add form item component

---
+++ src/components/forms/form-item.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  htmlFor?: string
+  className?: string
+  label?: string
+  labelClassName?: string
+  children?: React.ReactNode
+}
+
+export default function FormItem (
+  { htmlFor, className = '', label, children, labelClassName }: Props
+) {
+  return (
+    <div className={`relative -z-10 ${className}`}>
+      <label htmlFor={htmlFor} className={`mb-2 block text-sm font-medium text-gray-900 dark:text-white ${labelClassName}`}>
+        {label}
+      </label>
+      {children}
+    </div>
+  )
+}
