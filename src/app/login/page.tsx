3fe3bc713602c2b8f9ae87bd5f1cf32727dc8de5
hecsolmay <hecsolmay@gmail.com>
2024-01-16 13:18:48 -0600

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,43 @@
+import SocialButton from '@/components/buttons/social-button'
+import Link from 'next/link'
+
+export default function Login () {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#0F2027] via-[#203A43] to-[#2C5364] p-4 py-16">
+
+      <div className="mx-auto flex max-w-sm overflow-hidden rounded-lg bg-white shadow-lg lg:max-w-4xl">
+
+        <div className="min-h-[70vh] w-full px-4 lg:w-1/2">
+          <div className='flex items-center gap-x-3 p-6 '>
+            <Link href="/">
+              <img className="h-14 object-cover" src="/assets/images/logo.png" alt="" />
+            </Link>
+            <h2 className="ml-4 flex-1 text-balance text-3xl font-semibold capitalize text-gray-700">Bienvenido</h2>
+          </div>
+          <p className="mb-4 text-pretty text-center text-base font-medium text-gray-700">Inicia sesión con tu cuenta</p>
+          <div className="relative mb-6">
+            <div className="absolute inset-0 flex items-center">
+              <div className="border-strong w-full border-t"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-sm text-gray-600">selecciona una</span>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-4 px-6'>
+
+            <SocialButton type='google'/>
+            <SocialButton type='github'/>
+            <SocialButton type='facebook'/>
+            <SocialButton type='microsoft'/>
+
+          </div>
+        </div>
+
+        <div className="hidden bg-cover bg-no-repeat lg:block lg:w-1/2"
+          style={{ backgroundImage: 'url(/assets/images/background-login.webp)' }}>
+        </div>
+      </div>
+    </div>
+  )
+}
