5fb6416cce6b6a953f6896ccbd3a110d7a6baf0f
hecsolmay <hecsolmay@gmail.com>
2024-01-23 13:26:27 -0600

    add info empty state

---
+++ src/components/products/info-empty-state.tsx
@@ -0,0 +1,16 @@
+import ProductInfoContainer from './info-container'
+import InfoFooter from './info-footer'
+import InfoHeader from './info-header'
+
+export default function InfoEmptyState () {
+  return (
+    <ProductInfoContainer>
+      <InfoHeader title={'Producto No encontrado'} />
+      <div className='flex flex-1 flex-col items-center justify-center gap-4'>
+        <img className='w-1/3 object-cover' src="/assets/images/empty-search.webp" alt="" />
+        <h2 className='text-balance text-3xl font-bold'>No se pudo encontrar el producto</h2>
+      </div>
+      <InfoFooter />
+    </ProductInfoContainer>
+  )
+}
