40f2aeaa45e846ca0724812806c4e3278cb769e8
hecsolmay <hecsolmay@gmail.com>
2024-01-29 12:18:39 -0600

    change dynamic in home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -7,8 +7,6 @@ import { getCategories } from '@/services/category'
 import { getProducts } from '@/services/products'
 import { type SearchParams } from '@/types'
 
-export const dynamic = 'force-dynamic'
-
 export default async function Home ({
   searchParams
 }: {
