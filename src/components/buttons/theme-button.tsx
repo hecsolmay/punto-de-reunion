d65c89f206babb2b2ecbeed16845e4f65074040c
hecsolmay <hecsolmay@gmail.com>
2024-02-11 11:47:01 -0600

    Fix server render in ThemeButton

--- src/components/buttons/theme-button.tsx
+++ src/components/buttons/theme-button.tsx
@@ -3,11 +3,19 @@
 import MoonIcon from '@/components/icons/moon'
 import SunIcon from '@/components/icons/sun'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export default function ThemeButton (
   { className }: { className?: string }
 ) {
   const { setTheme, theme } = useTheme()
+  const [isFirstRender, setIsFirstRender] = useState(false)
+
+  useEffect(() => {
+    setIsFirstRender(true)
+  }, [])
+
+  if (!isFirstRender) return null
 
   const handleClick = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
