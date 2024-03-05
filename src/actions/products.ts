45bf240ce7f99ddeb0f250c682bd02f4edaef50d
hecsolmay <hecsolmay@gmail.com>
2024-03-05 12:51:49 -0600

    add products server actions to create, update and delete

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,6 +1,12 @@
 'use server'
 
+import { confirmUploadImages } from '@/actions/image'
 import prisma from '@/libs/prisma'
+import { productSchema, type ProductSchema } from '@/schemas/product'
+import { getCategoryById } from '@/services/category'
+import { getOrganizationById } from '@/services/organizations'
+import { getProductById } from '@/services/products'
+import { revalidateTag } from 'next/cache'
 
 interface FilterProducts {
   search?: string
@@ -51,6 +57,8 @@ export async function deleteProduct (productId: string) {
       }
     })
 
+    revalidateTag('products')
+
     return {
       data: productUpdated,
       success: true
@@ -60,6 +68,188 @@ export async function deleteProduct (productId: string) {
     return {
       error: 'Error al eliminar el producto',
       status: 500
+    }
+  }
+}
+
+interface CreateProduct extends ProductSchema {
+  organizationId: string
+  categories: string[]
+  images: string[]
+}
+
+export async function createProduct (newProduct: CreateProduct) {
+  const { categories = [], organizationId, images = [], ...rest } = newProduct
+
+  const noRepeatedCategories = Array.from(new Set(categories))
+
+  const result = productSchema.safeParse(rest)
+
+  if (!result.success) {
+    return {
+      success: false,
+      error: result.error
+    }
+  }
+
+  try {
+    const organization = await getOrganizationById(organizationId)
+
+    if (organization === null) {
+      return {
+        success: false,
+        error: 'Organization not found'
+      }
+    }
+
+    const categoriesPromise = noRepeatedCategories.map(async (categoryId) => {
+      return await getCategoryById(categoryId)
+    })
+
+    const categoriesResponse = await Promise.all(categoriesPromise)
+    const noNulleableCategories = categoriesResponse.filter((category) => category !== null)
+
+    if (noNulleableCategories.length !== noRepeatedCategories.length) {
+      return {
+        success: false,
+        error: 'Category not found'
+      }
+    }
+
+    const imageResponse = await confirmUploadImages(images)
+    const noNulleableImages = imageResponse ?? []
+
+    const { available, ...data } = result.data
+
+    const productCreated = await prisma.products.create({
+      data: {
+        ...data,
+        status: available ? 'AVAILABLE' : 'UNAVAILABLE',
+        organizationId,
+        categories: {
+          create: noNulleableCategories.map(c => ({ categoryId: c?.id ?? '' }))
+        },
+        images: {
+          create: noNulleableImages.map(image => ({ imageUrl: image ?? '' }))
+        }
+      }
+    })
+
+    revalidateTag('products')
+
+    return {
+      success: true,
+      data: productCreated
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      error: 'Error al crear el producto'
+    }
+  }
+}
+
+interface UpdateProduct extends ProductSchema {
+  categories: string[]
+  images: string[]
+}
+
+export async function updateProduct (productId: string, updateProduct: UpdateProduct) {
+  const { images = [], categories = [], ...body } = updateProduct
+  const result = productSchema.safeParse(body)
+
+  if (!result.success) {
+    return {
+      success: false,
+      error: result.error
+    }
+  }
+
+  const { available, ...data } = result.data
+
+  try {
+    const prevProduct = await getProductById(productId)
+
+    if (prevProduct === null) {
+      return {
+        success: false,
+        error: 'Product not found'
+      }
+    }
+
+    const noRepeatedCategories = Array.from(new Set(categories))
+    const categoriesPromise = noRepeatedCategories.map(async (categoryId) => {
+      return await getCategoryById(categoryId)
+    })
+
+    const categoriesResponse = await Promise.all(categoriesPromise)
+
+    const noNulleableCategories = categoriesResponse.filter((category) => category !== null)
+
+    if (noNulleableCategories.length !== noRepeatedCategories.length) {
+      return {
+        success: false,
+        error: 'Category not found'
+      }
+    }
+
+    const deletedImages = prevProduct.images.filter(image => !images.includes(image.imageUrl))
+    const deletedCategories = prevProduct.categories.filter(category => !noNulleableCategories.some(c => c?.id === category.categoryId))
+
+    const newImages = images.filter(image => !prevProduct.images.some(i => i.imageUrl === image))
+    await confirmUploadImages(newImages)
+    const newCategories = noNulleableCategories.filter(category => !prevProduct.categories.some(c => c.categoryId === category?.id))
+
+    const deleteImagePromise = prisma.productImages.deleteMany({
+      where: {
+        id: {
+          in: deletedImages.map(image => image.id)
+        }
+      }
+    })
+
+    const deleteCategoriesPromise = prisma.productsCategories.deleteMany({
+      where: {
+        id: {
+          in: deletedCategories.map(category => category.id)
+        }
+      }
+    })
+
+    const updatePromise = prisma.products.update({
+      data: {
+        ...data,
+        status: available ? 'AVAILABLE' : 'UNAVAILABLE',
+        categories: {
+          create: newCategories.map(c => ({ categoryId: c?.id ?? '' }))
+        },
+        images: {
+          create: newImages.map(image => ({ imageUrl: image ?? '' }))
+        }
+      },
+      where: {
+        id: productId
+      }
+    })
+
+    const [updatedProduct] = await prisma.$transaction([
+      updatePromise,
+      deleteImagePromise,
+      deleteCategoriesPromise
+    ])
+
+    revalidateTag('products')
+
+    return {
+      success: true,
+      data: updatedProduct
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      error: 'Error al crear el proyecto'
     }
   }
 }
