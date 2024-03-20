c50ce98683e44b44eb58718386e17dc39ba0e253
hecsolmay <hecsolmay@gmail.com>
2024-03-20 12:37:56 -0600

    add canvas confetti to success component

--- src/components/payments/success.tsx
+++ src/components/payments/success.tsx
@@ -1,4 +1,11 @@
+import CanvasConfetti from 'canvas-confetti'
+import { useEffect } from 'react'
+
 export default function SuccessPayment () {
+  useEffect(() => {
+    CanvasConfetti()
+  }, [])
+
   return (
     <p>Success</p>
   )
