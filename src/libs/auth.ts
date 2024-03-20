42f80b8974da98a863eec4037207665103f9ef15
hecsolmay <hecsolmay@gmail.com>
2024-03-19 20:17:42 -0600

    add try catch statement on getUserSession function

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -3,23 +3,28 @@ import prisma from '@/libs/prisma'
 import { type Prisma } from '@prisma/client'
 
 export async function getUserSession () {
-  const supabase = createServerSupabaseClient()
-  const { data: { session } } = await supabase.auth.getSession()
+  try {
+    const supabase = createServerSupabaseClient()
+    const { data: { session } } = await supabase.auth.getSession()
 
-  if (session === null) {
+    if (session === null) {
+      return null
+    }
+
+    const user = await prisma.users.findFirst({
+      include: {
+        organizations: true
+      },
+      where: {
+        id: session.user.id
+      }
+    })
+
+    return user
+  } catch (error) {
+    console.error(error)
     return null
   }
-
-  const user = await prisma.users.findFirst({
-    include: {
-      organizations: true
-    },
-    where: {
-      id: session.user.id
-    }
-  })
-
-  return user
 }
 
 export type UserSession = Prisma.PromiseReturnType<typeof getUserSession>
